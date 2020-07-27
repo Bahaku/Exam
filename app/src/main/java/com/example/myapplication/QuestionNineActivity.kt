6b5d6ee33c78672ea7ed11d87f4b5943b7721d36
bahak <bahako455@gmail.com>
2020-07-27 21:31:02 +0600

    exam

---
+++ app/src/main/java/com/example/myapplication/QuestionNineActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class QuestionNineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //Тостик 13 задание
+        Toast.makeText(this, "Типо да", Toast.LENGTH_LONG).show()    }
+}
